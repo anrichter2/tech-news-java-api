9af9cc32bca9e80fb185b013cdedd35f7eef2152
anrichter <anrichter2@gmail.com>
2025-07-30 23:22:21 -0500

    Made class models that will correspond to the post, users, comments, and votes that will be used by the database to store information.

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,21 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int num1 = 2;
+        double num2 = 5.99;
+        String word = "cat";
+
+        // calculated variable sum is a double
+        double sum = num1 + num2;
+
+        // calculated variable worInt is a String
+        String wordInt = word + num1;
+
+        // value of sum is 7.99
+        System.out.println(sum);
+
+        // value of wordInt is cat2
+        System.out.println(wordInt);
+    }
+}
